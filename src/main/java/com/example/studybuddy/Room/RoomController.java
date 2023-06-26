c10e8aeb036c321eab3e1f4f145b343548af5969
홍서정 <94052799+hsj517@users.noreply.github.com>
2023-06-27 01:39:43 +0900

    feat : 스터디룸 페이지로 이동

--- src/main/java/com/example/studybuddy/Room/RoomController.java
+++ src/main/java/com/example/studybuddy/Room/RoomController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -25,6 +26,10 @@ public class RoomController {
         return "mystudy";
     }
 
+    @GetMapping("/studyroom/{id}")
+    public String getStudyRoom(@PathVariable("id") int roomId){
+        return "roomMain";
+    }
     //스터디룸 생성
     @GetMapping("/create")
     public String create() {
